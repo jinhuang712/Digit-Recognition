0fec957fe6d134a6677ad9bddf5e279aa7e58933
Jin Huang <jinhuang696@gmail.com>
2019-06-01 21:30:53 +0800

    added clear button

--- code/web/main.js
+++ code/web/main.js
@@ -1,33 +1,40 @@
+let canvas;
+let ctx;
+let painting = false;
+
 window.onload = function () {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-
-    let painting = false;
-
-    function startPainting(e) {
-        painting = true;
-        draw(e);
-    }
-
-    function finishPainting() {
-        painting = false;
-        ctx.beginPath();
-    }
-
-    function draw(e) {
-        if (!painting) return;
-
-        ctx.lineWidth = 10;
-        ctx.lineCap = "round";
-
-        ctx.lineTo(e.clientX, e.clientY);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
-    }
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
     // EventListeners
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", finishPainting);
     canvas.addEventListener("mousemove", draw);
+
+    let clear = document.getElementById("clear");
+    clear.onclick = function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
 };
+
+function startPainting(e) {
+    painting = true;
+    draw(e);
+}
+
+function finishPainting() {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+}
