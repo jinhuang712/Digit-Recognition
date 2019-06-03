c270767ef35bf22c3dd96d36894968e5096c0430
Jin Huang <jinhuang696@gmail.com>
2019-06-03 21:10:03 +0800

    fixed initial mouse move induced undefined bound error

--- code/web/draw.js
+++ code/web/draw.js
@@ -4,6 +4,11 @@ function load_canvas() {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", finishPainting);
     canvas.addEventListener("mousemove", draw);
+
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    canvas.style.cursor = "crosshair";
+
     let clear = document.getElementById("clear");
     clear.onclick = function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -11,10 +16,13 @@ function load_canvas() {
         small_canvas.getContext("2d").clearRect(0, 0, small_canvas.width, small_canvas.height);
     };
 
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-
-    canvas.style.cursor="crosshair";
+    document.onmousemove = function (e) {
+        let rect = canvas.getBoundingClientRect();
+        if (e.clientX >= rect.right || e.clientX <= rect.left ||
+            e.clientY >= rect.bottom || e.clientY <= rect.top) {
+            finishPainting();
+        }
+    };
 }
 
 function startPainting(e) {
@@ -46,24 +54,13 @@ function check_if_canvas_empty() {
     return sum === 0;
 }
 
-// todo Uncaught TypeError: Cannot read property 'getBoundingClientRect' of undefined
-//      at HTMLDocument.document.onmousemove
-//      on initialized
-document.onmousemove = function (e) {
-    let rect = canvas.getBoundingClientRect();
-    if (e.clientX >= rect.right || e.clientX <= rect.left ||
-        e.clientY >= rect.bottom || e.clientY <= rect.top) {
-        finishPainting();
-    }
-};
-
 function display_input_tensor() {
     let imageData_scaled = scale(ctx.getImageData(0, 0, canvas.width, canvas.height));
     let small_canvas = document.getElementById("small-canvas");
     let newImageData = small_canvas.getContext("2d").createImageData(28, 28);
     for (let i = 0; i < 28; i++) {
         for (let j = 0; j < 28; j++) {
-            newImageData.data[i*4*28 + j*4 + 3] = imageData_scaled[i][j];
+            newImageData.data[i * 4 * 28 + j * 4 + 3] = imageData_scaled[i][j];
         }
     }
     small_canvas.getContext("2d").putImageData(newImageData, 0, 0);
